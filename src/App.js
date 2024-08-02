b52fa49e5e5b4b376b0bb57018d2bc21292f0c1d
Ishan-Sandaruwan <sadaruwan0427@gmail.com>
2024-08-02 20:50:27 +0530

    Exercise 1: Create a functional component that accepts a name prop and displays a greeting message.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
+import Ex1_Func from "./components/day1-2/Ex1_Func"
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <Ex1_Func name="saman" />
   )
 }
---
+++ src/components/day1-2/Ex1_Func.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Ex1_Func(props) {
+  return (
+    <div>Hello , {props.name}</div>
+  )
+}
